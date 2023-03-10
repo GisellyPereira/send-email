ce57bef9b25fb0389a4816ac9ba275da87a14bb6
Giselly Pereira <giselly.avp@gmail.com>
2023-03-10 20:38:53 -0300

    mudando style

--- src/App.js
+++ src/App.js
@@ -1,74 +1,90 @@
-import { useState } from 'react';
-import './App.css';
-import emailjs from '@emailjs/browser'
+import { useState } from "react";
+import "./App.css";
+import emailjs from "@emailjs/browser";
+import Image from "./componentes/imgs/woman.png";
 
 function App() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-
-  function sendEmail(e){
+  function sendEmail(e) {
     e.preventDefault();
 
-    if(name === '' || email === '' || message === ''){
-      alert('Preencha todos os campos');
+    if (name === "" || email === "" || message === "") {
+      alert("Preencha todos os campos");
       return;
-    };
+    }
 
     const templateParams = {
       from_name: name,
       message: message,
       email: email,
-    }
+    };
 
-    emailjs.send('service_xx2gdp2', 'template_bvpzog3', templateParams, 'WPguzsMv5yNYhkTxQ')
-    .then((response) =>{
-      console.log('email enviado', response.status, response.text);
-      setName('')
-      setEmail('')
-      setMessage('')
-    },(err) =>{
-      console.log('Email não enviado', err);
-    })
+    emailjs
+      .send(
+        "service_xx2gdp2",
+        "template_bvpzog3",
+        templateParams,
+        "WPguzsMv5yNYhkTxQ"
+      )
+      .then(
+        (response) => {
+          console.log("email enviado", response.status, response.text);
+          setName("");
+          setEmail("");
+          setMessage("");
+        },
+        (err) => {
+          console.log("Email não enviado", err);
+        }
+      );
   }
 
   return (
-    <div>
     <div className="container">
+      <div className="contain-flex">
+        <div className="part-right">
+          <h1 className="title">Get in Touch</h1>
+          <p className="text">We are here for you! How can we help you?</p>
 
-      <form className="form" onSubmit={sendEmail}>
-        <h1 className="title">CONTATO</h1>
-        <label className="label">Nome</label>
-        <input 
-          className="input"
-          type="text"
-          placeholder="Digite seu nome"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
+          <form className="form" onSubmit={sendEmail}>
+          
+          <input
+            className="input"
+            type="text"
+            placeholder="Enter your name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+
+          
+          <input
+            className="input"
+            type="email"
+            placeholder="Enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+
+          
+          <textarea
+            className="textarea"
+            placeholder="Message"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+
+          <input className="button" type="submit" value="Submit" />
+        </form>
+        </div>
         
-        <label className="label">E-mail</label>
-        <input 
-          className="input"
-          type="email"
-          placeholder="Digite seu email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
 
-        <label className="label">Mensagem</label>
-        <textarea 
-          className="textarea"
-          placeholder="Digite sua mensagem..."
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-        />
-
-        <input className="button" type="submit" value="Enviar" />
-      </form>
-
-    </div>
+        <div>
+          <img className="img" src={Image} alt="Imagem de uma mulher" />
+        </div>
+      </div>
     </div>
   );
 }
